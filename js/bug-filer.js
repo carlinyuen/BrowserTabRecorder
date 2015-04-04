345df982c5390213b8a662c3173e58ef245be5c4
Carlin <email.me@carlinyuen.com>
2015-04-04 12:08:47 -0700

    showing cursor somewhow

---
+++ js/bug-filer.js
@@ -0,0 +1,70 @@
+console.log('Initializing Bug-Filer');
+
+$(function()
+{
+    // Variables
+    var IMAGE_CURSOR = 'images/cursor.png'
+        , IMAGE_CURSOR_PRESSED = 'images/cursor_pressed.png'
+        
+        , thumbnail = $(document.createElement('div'))
+            .addClass('bug-filer-thumbnail')
+            .css({
+                'position': 'fixed',
+                'right': '16px',
+                'bottom': '16px',
+                'width': '256px',
+                'border': '4px solid #000'
+            })
+        , cursor = $(document.createElement('div'))
+            .addClass('bug-filer-cursor')
+            .css({
+                'width': '48px',
+                'height': '48px',
+                'position': 'absolute',
+                'z-index': '999',
+                'background': '#000',
+                //'background': 'url(images/cursor.png) center center no-repeat'
+            }).appendTo('body')
+        , recording = false
+    ;
+
+    // Listener
+    $(document).mousemove(function (event) {
+        console.log('mousemove');
+        if (recording) {
+            console.log('cursor');
+            cursor.show().css({
+                'top': event.pageY - 24,
+                'left': event.pageX - 24
+            });
+        } else {
+            cursor.hide();
+        }
+    });
+
+    chrome.runtime.onMessage.addListener(function (request, sender, response) 
+    {
+        switch (request.request)
+        {
+            case "recordingStarted":
+                recording = true;
+                break;
+
+            case "recordingStopped":
+                recording = false;
+                break;
+
+            default:
+                break;
+        }
+    });
+
+
+    // Functions
+
+    // Create thumbnail from recording source (image / video)
+    function createThumbnail(sourceURL)
+    {
+        // TODO
+    }
+});
