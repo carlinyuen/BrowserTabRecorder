f2ef4b190e6a56f1d1d1a72a8302aeb69283a4f7
Carlin <email.me@carlinyuen.com>
2015-04-14 21:24:57 -0400

    formatting dates

--- js/bug-filer.js
+++ js/bug-filer.js
@@ -275,8 +275,8 @@ $(function()
             + '.' + ('0' + (date.getMonth() + 1)).slice(-2)  
             + '.' + ('0' + date.getDate()).slice(-2)  
             + '-' + ('0' + date.getHours()).slice(-2)  
-            + "'" + ('0' + date.getMinutes()).slice(-2) 
-            + '"' + ('0' + date.getSeconds()).slice(-2) 
+            + "_" + ('0' + date.getMinutes()).slice(-2) 
+            + '_' + ('0' + date.getSeconds()).slice(-2) 
         ;
     }
 
