8d13e14433b3ac5441058d82e618a94666bf716a
Carlin <email.me@carlinyuen.com>
2015-05-26 11:04:32 +0800

    wiring up saving / loading of settings

--- js/options.js
+++ js/options.js
@@ -12,17 +12,27 @@ $(function()
     {
         console.log('init()');
 
-        attachListners();
+        attachListeners();
         reloadSettings();
     }
 
     // Attach listeners to UI elements
     function attachListeners()
     {
+        console.log('attachListeners()');
+
+        // Track changes to checkboxes
+        $('input[type="checkbox"]').on('change', function() {
+            saveSettings();
+        });
+
         // Track range input changes
-        $('input[type="range"]').on('mouseup', function() {
+        $('input[type="range"]').on('mouseup', function() 
+        {
             this.blur();
-        }).on('mousedown input', function() {
+            saveSettings();
+        })
+        .on('mousedown input', function() {
             updateRangeOutput(this);
         });
     }
@@ -30,6 +40,8 @@ $(function()
     // Reload settings
     function reloadSettings()
     {
+        console.log('reloadSettings()');
+
         chrome.storage.sync.get(KEY_STORAGE_SETTINGS, function(data)
         {
             if (chrome.runtime.lastError) 
@@ -39,13 +51,55 @@ $(function()
             } 
             else    // Success, update settings
             {	
+                console.log('Load success!', data);
+
                 // Update input fields
-                $.each(data, function(key, value) {
-                    $('#' + key).val(value);
+                var settings = data[KEY_STORAGE_SETTINGS];
+                $.each(settings, function(key, value) 
+                {
+                    var el = $('#' + key);
+                    if (el.attr('type') == 'checkbox') {
+                        el[0].checked = value;
+                    } else {
+                        el.val(value);
+                    }
                 });
                 
                 // Update range outputs
                 updateRangeOutput($('#gifFrameSkipSetting')[0]);
+            }
+        });
+    }
+
+    // Save settings
+    function saveSettings()
+    {
+        console.log('saveSettings()');
+
+        // Collect settings from inputs
+        var settings = {};
+        $('input').each(function(index, element) 
+        {
+            if ($(element).attr('type') == 'checkbox') {
+                settings[element.id] = element.checked;
+            } else {
+                settings[element.id] = element.value;
+            }
+        });
+
+        // Save with key
+        var data = {};
+        data[KEY_STORAGE_SETTINGS] = settings;
+        chrome.storage.sync.set(data, function()
+        {
+            if (chrome.runtime.lastError) 
+            {
+                console.log(chrome.runtime.lastError);
+                alert('Could not save settings!');
+            } 
+            else    // Success
+            {
+                console.log('Save success!', data);
             }
         });
     }
